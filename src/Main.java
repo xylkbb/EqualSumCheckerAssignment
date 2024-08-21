979132c92288f02d081fc21d43caeaabcd942187
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-21 13:24:50 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+
+
+    }
+
+    public static boolean hasEqualSum(int num1, int num2, int num3){
+        int AnswerToAddition = num1 +num2;
+        return AnswerToAddition == num3;
+
+
+    }
+
+
+    }
+
+
